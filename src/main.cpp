6c5d7eecba6ead5ebd765aab8186b0f8c05092e2
Cihan Eran <erancihan@gmail.com>
2023-07-01 16:15:08 +0000

    init project
    start walking through helloworld window

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "core/stdafx.h"
+#include "application/Win32Application.h"
+
+
+_Use_decl_annotations_
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    return 0;
+}
